9c5e8d5b1b8cc3db01a33b4a980e68754fc268ff
Karina Ruiz Marcial <marcialkarina48@gmail.com>
2025-01-15 14:32:58 -0600

    arreglando pagina principal

--- my_second_project/products/urls.py
+++ my_second_project/products/urls.py
@@ -5,5 +5,5 @@ from .views import  ProductListView, ProductDetailView, buscar_producto
 urlpatterns = [
     path("", ProductListView.as_view(), name="list_product"),
     path("detalle/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
-    path('buscar/',  buscar_producto, name='buscar_producto')
+    path('buscar/', buscar_producto, name='buscar_producto')
 ]
