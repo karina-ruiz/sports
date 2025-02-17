2e938be7f9800ef1d57430b205fc41e47f16dbb6
Karina Ruiz Marcial <marcialkarina48@gmail.com>
2025-02-17 09:26:47 -0600

    carrito funcionando

--- my_second_project/my_second_project/urls.py
+++ my_second_project/my_second_project/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("productos/", include("products.urls")),
+    path('productos/', include("products.urls")),
     path('basket/', include('basket.urls'))
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
